08fddd7b3e85db511ffd0d6840eb15261c6a32a8
Satyajeet Ramnit <satyajeetramnit@gmail.com>
2026-04-21 18:07:22 +0530

    fix: mark dashboard page as client component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import KanbanBoard from "@/components/KanbanBoard";
 import ApplicationDrawer from "@/components/ApplicationDrawer";
 import AddApplicationModal from "@/components/AddApplicationModal";
